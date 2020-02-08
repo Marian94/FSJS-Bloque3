5306cb1692502b976c7aa5210f64613a1a055062
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-07 22:47:19 -0600

    Ejercicio 2

--- Bloque03Clase14/b03c14ej2.js
+++ Bloque03Clase14/b03c14ej2.js
@@ -9,6 +9,50 @@
 */
 
 //tu código va aquí vvv
+/**
+ * OPCION 1
+ * Declararemos 3 variables diferentes por cada alumno.
+ * Una sera de tipo constante (Nombre), las otras 2 seran de tipo let (edad y SO).
+ */
+const nombreA = "Mariana";
+let ageA = "25";
+let WindowsA = true;
+const nombreB = "Gerardo";
+let ageB = "40(?)";
+let WindowsB = false;
+const nombreC = "Jorge"; 
+let ageC = "31(?)";
+let WindowsC = true;
+
+console.log(`Nombre: ${nombreA}\nEdad: ${ageA}\nWindows: ${WindowsA}\n________________`);
+console.log(`Nombre: ${nombreB}\nEdad: ${ageB}\nWindows: ${WindowsB}\n________________`);
+console.log(`Nombre: ${nombreC}\nEdad: ${ageC}\nWindows: ${WindowsC}\n________________`);
+
+ /**
+  * OPCION 2
+  * Se puede hacer un arreglo de objetos, el cual solo almacenara objetos de personas
+  */
+ //Declaramos un arreglo vacio.
+let Alumnos = [];
+//Agregamos objetos de alumnos
+Alumnos.push({
+    nombre:"Mariana",
+    edad: "25",
+    windows: true
+});
+Alumnos.push({
+    nombre:"Jorge",
+    edad: "31(?)",
+    windows: false
+});
+Alumnos.push({
+    nombre:"Gerardo",
+    edad: "40(?)",
+    windows: false
+});
+//Desplegamos mensaje
+console.log(Alumnos);
+
 
 //tu código va aquí ^^^
 
