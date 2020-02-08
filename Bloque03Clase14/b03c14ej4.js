88d071fba93d4d583d1bda7b5b41116aa51daf0a
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-07 22:57:47 -0600

    Ejercicio 4

--- Bloque03Clase14/b03c14ej4.js
+++ Bloque03Clase14/b03c14ej4.js
@@ -13,9 +13,33 @@ let v3 = "hola"
 let test;
 
 //tu código va aquí vvv
+/**
+ * parseInt convierte el valor de una variable a un numero
+ * toString() convierte el valor de una variable a una cadena
+ * Boolean() convierte el valor de una variable a un booleano
+ */
+test = parseInt(v1);
+console.log(test+" es del tipo "+typeof(test));
+test = v1.toString();
+console.log(test+" es del tipo "+typeof(test));
+test = Boolean(v1);
+console.log(test+" es del tipo "+typeof(test)+ '\n__________________________');
+
+test = parseInt(v2);
+console.log(test+" es del tipo "+typeof(test));
+test = v2.toString();
+console.log(test+" es del tipo "+typeof(test));
+test = Boolean(v2);
+console.log(test+" es del tipo "+typeof(test)+ '\n__________________________');
+
+test = parseInt(v3);
+console.log(test+" es del tipo "+typeof(test));
+test = v3.toString();
+console.log(test+" es del tipo "+typeof(test));
+test = Boolean(v3);
+console.log(test+" es del tipo "+typeof(test)+ '\n__________________________');
 
 //tu código va aquí ^^^
 
-console.log(test+" es del tipo "+typeof(test));
 
 //eof
