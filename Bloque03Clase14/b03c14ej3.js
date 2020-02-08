a7ceaa3e7af2c0ac903cb3a28007dd39a6357daf
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-07 22:20:06 -0600

    Ejercicio 3

--- Bloque03Clase14/b03c14ej3.js
+++ Bloque03Clase14/b03c14ej3.js
@@ -5,11 +5,19 @@
     Usa const o let según sea necesario.
     Verifica si el enunciado completo se imprime en pantalla correctamente.
 */
-
+/**
+ * La unica variable que no cambia es el nombre, por ende esa variable sera de tipo const
+ * Las variables comida y juego cambiaran, por ende son de tipo let
+ */
+const nombre = "Mariana";
+let comida = "Panini";
+let juego = "Zelda BoW";
 console.log("Me llamo: "+nombre);
 console.log("Hoy comí: "+comida);
+comida = "Aguachile";
 console.log("Mañana comeré: "+comida);
 console.log("Ahorita ando jugando: "+juego);
+juego = "Salem o Catan";
 console.log("Cuando lo acabe, jugaré: "+juego);
 
 //eof
