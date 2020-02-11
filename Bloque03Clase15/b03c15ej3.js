12dc8a7a0a66e40d4c1058b206675e9ed5b68de8
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-10 21:47:30 -0600

    Ejercicio 3

--- Bloque03Clase15/b03c15ej3.js
+++ Bloque03Clase15/b03c15ej3.js
@@ -8,11 +8,18 @@
 function getRandomNumber(min, max) {
     return Math.floor( Math.random() * (max-min+1) ) + min;
 }
-
-getRandomNumber(1000000000, 9999999999) / 1000000000;
-
+function getRandomDecimals(number, decimals){
+    console.log(`Numero: ${number}`);
+    console.log(`Trunca todos los decimales (trunc): ${Math.trunc(number)}`);
+    console.log(`Redondea hacia arriba quitando todos los decimales (ceil): ${Math.ceil(number)}`);
+    console.log(`Redondea hacia abajo quitando todos los decimales (floor): ${Math.floor(number)}`);
+    console.log(`Redondea de forma estándar quitando todos los decimales (round): ${Math.round(number)}`);
+    console.log(`Mostrar numero con ${decimals} decimales (el numero de decimales es random): ${number.toFixed(decimals)}`);
+}
+const randomNumber = getRandomNumber(1000000000, 9999999999) / 1000000000;
+const randomDecimals = getRandomNumber(1, 8);
 //tu código va aquí vvv
-
+getRandomDecimals(randomNumber,randomDecimals)
 //tu código va aquí ^^^
 
 //eof
