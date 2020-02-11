461b34d4116c3edb205de92b9e959a2e67898a24
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-10 21:32:44 -0600

    Ejercicio 1

--- Bloque03Clase15/b03c15ej1.js
+++ Bloque03Clase15/b03c15ej1.js
@@ -13,6 +13,24 @@
 
 //tu código va aquí vvv
 
+// -->Por orden de jerarquia de operaciones, se realiza primero las exponentes
+const exponentes = 79 ** 2;
+
+// --> La multiplicacion, division y modulos tienen el mismo rango de jerarquia
+// 84 + 96 - 25 * exponentes * 59 % 69 + 21 / 53
+const multiplicacion = 25 * exponentes *59;
+// 84 + 96 - multiplicacionA % 69 + 21 / 53
+const modulo = multiplicacion % 69;
+// 84 + 96 - modulo + 21 / 53
+const division = 21 / 53;
+// 84 + 96 - modulo + division
+
+// -->Por ultimo, se realizan las sumas y restas
+const resultado = 84 + 96 - modulo + division;
+
+//La propiedad toFixed(#), conserva el numero de decimales deseados
+console.log(resultado.toFixed(2));
+
 //tu código va aquí ^^^
 
 //eof
