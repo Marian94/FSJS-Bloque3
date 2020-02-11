ee5094a74a60f46ef41ada1410e80720ab66baa7
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-10 21:33:15 -0600

    Ejercicio 2

--- Bloque03Clase15/b03c15ej2.js
+++ Bloque03Clase15/b03c15ej2.js
@@ -13,6 +13,25 @@ function getRandomNumber(min, max) {
 
 //tu código va aquí vvv
 
+//Esta funcion imprime el resultado si es un numero par (result = 0) o impar (result = 1)
+function parImpar(result, randomNumber){
+    if( resultado != 1){
+    console.log(`${randomNumber} es par`)
+    } else{
+    console.log(`${result} es impar`)
+    }
+}
+
+//Se crea dentro de un bucle (de 10 vueltas), para generar 10 numeros aleatorios 
+for( let i = 0; i < 10; i++){
+    const randomNumber = getRandomNumber(0, 100);
+    // Se toma el numero generado y se hace la operacion del modulo (%) para ver si es numero par o impar
+    const result = numeroRandom % 2;
+    //Se imprimen los resultados
+    parImpar(result, randomNumber);
+}
+
+
 //tu código va aquí ^^^
 
 //eof
