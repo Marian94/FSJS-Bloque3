ccf13b7555990c8f9475e8b4570596337aa36509
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-11 21:22:42 -0600

    Agregar boque try catch

--- Bloque03Clase16/b03c16ej3.js
+++ Bloque03Clase16/b03c16ej3.js
@@ -12,19 +12,22 @@ let resultados = "";
 let divresultados = document.getElementById("divresultados");
 
 function messageClick() {
+    try{
+        //usa term1.value y term2.value para obtener lo que escribes en los input.
+        //el string resultados debe describir todas las comparaciones que realices.
 
-    //usa term1.value y term2.value para obtener lo que escribes en los input.
-    //el string resultados debe describir todas las comparaciones que realices.
+        //tu código va aquí vvv
+        resultados = `[${term1} <= ${term2}] = ${term1<=term2} dependiendo del string ingresado, los elementos ingresados son de tipo string.\n
+                    [${term1} <= ${term2}] = ${parseInt(term1)<=parseInt(term2)} dependiendo el numero ingresado, los elementos ingresados son numeros ya que se convirtieron usando parseInt\n
+                    [Boolean(${term1})] = ${Boolean(term1)} ya que es una cadena string\n
+                    [Boolean(${term2})] = ${Boolean(parseInt(term2))} ya que es un numero, se convirtio con parseInt` ;
 
-    //tu código va aquí vvv
-    resultados = `[${term1} <= ${term2}] = ${term1<=term2} dependiendo del string ingresado, los elementos ingresados son de tipo string.\n
-                [${term1} <= ${term2}] = ${parseInt(term1)<=parseInt(term2)} dependiendo el numero ingresado, los elementos ingresados son numeros ya que se convirtieron usando parseInt\n
-                [Boolean(${term1})] = ${Boolean(term1)} ya que es una cadena string\n
-                [Boolean(${term2})] = ${Boolean(parseInt(term2))} ya que es un numero, se convirtio con parseInt` ;
+        //tu código va aquí ^^^
 
-    //tu código va aquí ^^^
-
-    divresultados.innerHTML = resultados;
+        divresultados.innerHTML = resultados;
+    } catch(e){
+        console.log(e);
+    }
 }
 
 //eof
