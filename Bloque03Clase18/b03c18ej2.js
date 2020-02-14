c1e26039208fa95561c8b90b46ca2553d586849b
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-02-13 20:48:12 -0600

    Ejercicio 2

--- Bloque03Clase18/b03c18ej2.js
+++ Bloque03Clase18/b03c18ej2.js
@@ -23,7 +23,34 @@ function messageClick() {
     //usa r.innerHTML para colocar el resultado en pantalla
 
     //tu código va aquí vvv
-
+    //Valid number es la expresion regular que verificara que lo que ingrese el usuario sea coherente a lo que se este pidiendo, en este caso solo se pueden ingresar numeros y valores flotantes
+    const validNumber = /^-?\d+(?:.\d+)?$/g;
+    let message = "Porfavor, evita:\n*Escribir mal un número\n*Dejar la calificacion en blanco\n*Escribir letras y/o caracteres especiales"; 
+    //Se verifica que el nombre contenga datos
+    if(calif.value.replace(/ /g, "") !== ""){
+        //Se verifica que los datos ingresados sean validos no incluyan numeros ni caracteres especiales)
+        if(validNumber.test(calif.value) === true ){
+            const calificacion = parseFloat(calif.value);
+            if(calificacion <= 100){
+                if (calificacion >= 0 && calificacion <= 9 ) message = "Vete a turismo";
+                else if (calificacion >= 10 && calificacion <= 59) message = "tronaste";
+                else if (calificacion >= 60 && calificacion <= 69) message = "panzaste";
+                else if (calificacion >= 70 && calificacion <= 89) message = "ya eres libre";
+                else if (calificacion >= 90 && calificacion <= 100) message = "nerd";
+            }else{
+                message = "La calificacion ingresada es mayor a 100";
+            }
+            alert(message);
+        } //if validNumber
+        else{
+            //Se despliega un mensaje de error
+            alert(message);
+        }//else validNumber
+    } //if name
+     else{
+         //Se despliega un mensaje de error
+        alert(message);
+    } // else name
     //tu código va aquí ^^^
 }
 
