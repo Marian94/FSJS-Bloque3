3c98fd738e78e2e3ff3a785c3bf334c2397628c3
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-03-04 20:37:48 -0600

    Ejercicio clase 27

--- Bloque03Clase27/b03c27ej1.js
+++ Bloque03Clase27/b03c27ej1.js
@@ -9,7 +9,39 @@
 */
 
 //tu código va aquí vvv
+class Pajaro {
+    constructor() {
+        this.propDePajaro = "Soy pájaro";
+    }
+}
 
+class Pollo extends Pajaro {
+    constructor() {
+        super();
+        this.propDePollo = "Soy pollo";
+    }
+}
+
+const po = new Pollo();
+po.__proto__.propDeProtoPollo = "Soy proto de Pollo";
+po.__proto__.__proto__.propDeProtoPajaro = "Soy proto de Pajaro";
+po.__proto__.__proto__.__proto__.propDeProtoObjeto ="Soy proto de Object";
+
+function printProtoChain(obj, ind="") {
+    ind2 = ind+"    ";
+    if(obj==null) {
+        console.log("\n"+ind+"null");
+        return;
+    }
+    console.log("\n"+ind+obj.constructor.name+" : {\n");
+    for(let p in obj) {
+        console.log(`${ind2}"${p}" : "${obj[p]}",`);
+    }
+    printProtoChain(Object.getPrototypeOf(obj), ind2);
+    console.log(ind+"}"); //cerrar bloque de objeto
+}
+
+printProtoChain(po);
 //tu código va aquí ^^^
 
 //eof
