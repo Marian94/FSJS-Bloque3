3dd37b288b8fecfdc5f978df88de93417ef7dbc7
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-03-10 19:55:59 -0600

    Ejercicio clase 36

--- Bloque03Clase36/b03c36ej1.js
+++ Bloque03Clase36/b03c36ej1.js
@@ -8,7 +8,39 @@
 */
 
 //tu código va aquí vvv
+//Creamos una funcion que recibe una profesion
+function externa (profesion = ""){
+    //validamos que lo ingresado sea valido (sea un string)
+    if(typeof profesion === "string" && profesion !== ""){
+        //respuesta sera el string que retornaremos si cumplen todas las condiciones
+        let respuesta = profesion;
+        //retornamos una funcion que recibira un nombre  y retorna la profesion y el nombre ingresado
+        return function interna(nombre = ""){
+            //concatenamos la respuesta y la imprimimos 
+            respuesta += " "+ nombre;
+            console.log(respuesta);
+        }
+    }//si no, se retorna un 0
+    else{
+        return 0;
+    }
+}
+//Esta funcion validara que los datos ingresados en f sean los esperados
+function validation (f){
+    if(f  !== 0){
+        //Si lo ingresado es correcto, le mandamos a f un nombre
+        f("Juan");
+    }else{
+        //Si lo ingresado es incorrecto, mandamos un mensaje de error
+        console.log("Ingresa una cadena de texto");
+    }
+}
 
+let f = externa();
+validation (f);
+f = externa (123);
+validation (f);
+f = externa ("Chef");
+validation (f);
 //tu código va aquí ^^^
-
 //eof
