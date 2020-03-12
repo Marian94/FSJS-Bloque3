9e96902d115d3acc771cbe6277af627963188825
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-03-11 21:37:30 -0600

    Ejercicio clase 28

--- Bloque03Clase28/b03c28ej1.js
+++ Bloque03Clase28/b03c28ej1.js
@@ -10,7 +10,35 @@
 */
 
 //tu código va aquí vvv
+class Persona {
+    constructor(nombre, edad){
+    this.nombre = nombre;
+    this.edad = edad;
+    }
+    imprimeNombre(){
+        console.log(`Hola, me llamo ${this.nombre} soy de la clase Persona`);
+    }
+}
+class Hobbies extends Persona {
+    constructor(hobbie){
+        super();
+        this.hobbie = hobbie;
+    }
+    printObject(){
+        console.log(`Tengo ${this.edad} años y esta propiedad es heredada de la clase Persona, y me gusta ${this.hobbie}, este hobbie viene directo de la clase Hobbies`)
+    }
+}
+const obj = new Hobbies("Felting");
+obj.nombre = "Mariana";
+obj.edad = 25;
+obj.imprimeNombre();
+obj.printObject();
+console.log("------------------");
+const obj2 = new Hobbies("Cocina");
+obj2.nombre = "German";
+obj2.edad = 23;
+obj2.imprimeNombre();
+obj2.printObject();
 
 //tu código va aquí ^^^
-
 //eof
