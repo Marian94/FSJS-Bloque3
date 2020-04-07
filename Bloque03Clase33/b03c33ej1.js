ed199c22a26603732790c0f7812d362003749e40
MARIANA GONZALEZ ROMERO <marian.romero@ibm.com>
2020-04-06 19:23:47 -0500

    Clase 33 ejercicio 1

--- Bloque03Clase33/b03c33ej1.js
+++ Bloque03Clase33/b03c33ej1.js
@@ -11,6 +11,47 @@
 //tu código va aquí vvv
 
 
+
+class Libro {
+    constructor(paginas){
+        this.paginas = paginas;
+    }
+}
+
+class LibroTecnico extends Libro {
+    constructor(paginas){
+        super (paginas);
+        this.genero = "Ciencia";
+    }
+}
+
+const librito = new Libro("520");
+Object.getPrototypeOf(librito).name = "JS";
+
+function printProtoChain(obj, ind="") {
+
+    ind2 = ind+" ";
+
+    if(obj==null) {
+        console.log("\n"+ind+"null");
+        return;
+    }
+
+    console.log("\n"+ind+obj.constructor.name+" : {\n");
+    var objectSymbols = Object.getOwnPropertySymbols(obj);
+
+    //For of porque es un arreglo
+    for(let p of objectSymbols) {
+        console.log(p);
+    }
+
+    printProtoChain(Object.getPrototypeOf(obj), ind2);
+    console.log(ind+"}"); //cerrar bloque de objeto
+}
+
+const PAGINAS = Symbol("520");
+librito[PAGINAS] = 230;
+printProtoChain(librito);
 //tu código va aquí ^^^
 
 //eof
